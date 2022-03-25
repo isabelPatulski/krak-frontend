3eb95a7141c292c9f12f40a789d747956ec30d1a
Casper <casper.aakviststeengaard@gmail.com>
2022-03-25 09:35:14 +0100

    Fixed undefined dawaAutocomplete method

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-"use strict"
+export function setDawa(){
 dawaAutocomplete.dawaAutocomplete( document.getElementById("adresse"), {
   select: function(selected) {
     document.getElementById("valgtadresse").innerHTML= selected.tekst;
   }
 });
+}
